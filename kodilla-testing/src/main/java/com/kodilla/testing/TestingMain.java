6d9184df4b243843b7497715bf8102a6abfa1ee9
MariuszGebel <gebel@wp.pl>
2018-05-08 21:47:08 +0200

    Calculator added - First commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        //System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("First test OK!");
+        } else {
+            System.out.println("First test ERROR!!");
+        }
+
+        Calculator calc = new Calculator();
+
+        int calcResult = calc.add(3,5);
+        if (calcResult == 8) {
+            System.out.println("Adding OK!");
+        } else {
+            System.out.println("Adding ERROR!!");
+        }
+
+        calcResult = calc.subtact(5, 3);
+        if (calcResult == 2) {
+            System.out.println("Subtracting OK!");
+        } else {
+            System.out.println("Subtracting ERROR!!");
+        }
     }
 }
