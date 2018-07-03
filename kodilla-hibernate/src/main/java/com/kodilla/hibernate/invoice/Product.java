6101ef4853c2fb1b8421337ef7b8ec5af6e46f77
MariuszGebel <gebel@wp.pl>
2018-07-03 22:34:00 +0200

    13.3

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Product.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Product.java
@@ -1,40 +1,55 @@
 package com.kodilla.hibernate.invoice;
 
-//import javax.persistence.*;
-//import javax.validation.constraints.NotNull;
-//
-//@Entity
-//@Table(name = "PRODUCT")
-//public class Product {
-//    private int id;
-//    private String name;
-//
-//    public Product(){}
-//
-//    public Product(String name){
-//        this.name = name;
-//    }
-//
-//    @Id
-//    @GeneratedValue
-//    @NotNull
-//    @Column(name = "PRODUCT_ID", unique = true)
-//    public int getId(){
-//        return id;
-//    }
-//
-//    @NotNull
-//    @Column(name = "NAME")
-//    public String getName(){
-//        return name;
-//    }
-//
-//    private void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    private void setName(String name) {
-//        this.name = name;
-//    }
-//
-//}
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    private int id;
+    private String name;
+    private List<Item> items;
+
+    public Product(){}
+
+    public Product(String name){
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "PRODUCT_ID", unique = true)
+    public int getId(){
+        return id;
+    }
+
+    @NotNull
+    @Column(name = "NAME")
+    public String getName(){
+        return name;
+    }
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "product",
+            targetEntity = Item.class
+    )
+    public List<Item> getItems() {
+        return items;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setItems(List<Item> items){
+        this.items = items;
+    }
+}
